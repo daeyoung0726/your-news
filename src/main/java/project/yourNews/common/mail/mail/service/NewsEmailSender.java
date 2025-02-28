e85f0061b31fb76936e4b62da84fe8034ad9b803
DaeYoung0726 <pdy000726@naver.com>
2025-02-28 17:07:28 +0900

    refact: 메일 전송 책임 분리

--- src/main/java/project/yourNews/common/mail/mail/service/NewsEmailSender.java
+++ src/main/java/project/yourNews/common/mail/mail/service/NewsEmailSender.java
@@ -1,2 +1,53 @@
-package project.yourNews.common.mail.mail.service;public class NewsEmailSender {
+package project.yourNews.common.mail.mail.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import project.yourNews.common.mail.mail.MailContentBuilder;
+import project.yourNews.common.mail.mail.util.MailProperties;
+import project.yourNews.crawling.dto.EmailRequest;
+import project.yourNews.domains.notification.dto.NewsListDto;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NewsEmailSender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    public void sendNewsToMember(List<String> memberEmails, String newsName, String postTitle, String postURL) {
+        String mailContent = MailContentBuilder.buildNewsMailContent(newsName, postTitle, postURL);
+        sendEmail(memberEmails, newsName, mailContent);
+    }
+
+    public void sendNewsToMember(List<String> memberEmails, String newsName, List<NewsListDto> newsListDtos) {
+        String mailContent = MailContentBuilder.buildJobNewsMailContent(newsName, newsListDtos);
+        sendEmail(memberEmails, newsName, mailContent);
+    }
+
+    public void sendDailyNewsToMember(List<String> memberEmails, String newsName, List<NewsListDto> newsListDtos) {
+        String mailContent = MailContentBuilder.buildNewsMailContent(newsName, newsListDtos);
+        sendEmail(memberEmails, newsName, mailContent);
+    }
+
+    private void sendEmail(List<String> memberEmails, String newsName, String mailContent) {
+        for (String email : memberEmails) {
+            EmailRequest emailRequest = new EmailRequest(email, MailProperties.NEWS_SUBJECT, mailContent);
+            try {
+                rabbitTemplate.convertAndSend(exchangeName, routingKey, emailRequest);
+            } catch (Exception e) {
+                log.error("Failed to send email request to RabbitMQ : {}", newsName, e);
+            }
+        }
+    }
 }
--- src/main/java/project/yourNews/domains/notification/service/NotificationScheduler.java
+++ src/main/java/project/yourNews/domains/notification/service/NotificationScheduler.java
@@ -2,18 +2,14 @@ package project.yourNews.domains.notification.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import project.yourNews.common.mail.mail.MailContentBuilder;
-import project.yourNews.common.mail.mail.util.MailProperties;
-import project.yourNews.crawling.dto.EmailRequest;
+import project.yourNews.common.mail.mail.service.NewsEmailSender;
 import project.yourNews.domains.member.service.MemberService;
 import project.yourNews.domains.news.dto.NewsInfoDto;
 import project.yourNews.domains.news.service.NewsService;
-import project.yourNews.domains.notification.dto.NewsNotificationDto;
+import project.yourNews.domains.notification.dto.NewsListDto;
 
 import java.util.List;
 
@@ -24,15 +20,9 @@ import java.util.List;
 public class NotificationScheduler {
 
     private final NewsService newsService;
-    private final RabbitTemplate rabbitTemplate;
     private final NotificationService notificationService;
     private final MemberService memberService;
-
-    @Value("${rabbitmq.exchange.name}")
-    private String exchangeName;
-
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
+    private final NewsEmailSender newsEmailSender;
 
     @Scheduled(cron = "0 0 20 * * *")
     public void runDailyNewsNotificationJob() {
@@ -43,29 +33,12 @@ public class NotificationScheduler {
                 .map(NewsInfoDto::getNewsName)
                 .forEach(newsName -> {
 
-                    List<NewsNotificationDto> notificationDtos = notificationService.getAllNewsInfo(newsName);
+                    List<NewsListDto> newsListDtos = notificationService.getAllNewsInfo(newsName);
 
-                    if (!notificationDtos.isEmpty()) {
+                    if (!newsListDtos.isEmpty()) {
                         List<String> memberEmails = memberService.findEmailsByDailySubscribedNews(newsName);
-                        sendNewsToMember(memberEmails, newsName, notificationDtos);
+                        newsEmailSender.sendDailyNewsToMember(memberEmails, newsName, newsListDtos);
                     }
                 });
-    }
-
-    /* 소식 구독자에게 메일 보내기 */
-    private void sendNewsToMember(List<String> memberEmails, String newsName, List<NewsNotificationDto> notificationDtos) {
-        String mailContent = MailContentBuilder.buildNewsMailContent(newsName, notificationDtos);
-
-
-        for (String email : memberEmails) {
-            EmailRequest emailRequest = new EmailRequest(email, MailProperties.NEWS_SUBJECT, mailContent);
-
-            try {
-                rabbitTemplate.convertAndSend(exchangeName, routingKey, emailRequest);
-
-            } catch (Exception e) {
-                log.error("Failed to send email request to RabbitMQ : {}", newsName, e);
-            }
-        }
     }
 }
