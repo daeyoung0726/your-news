97a76484827c551df04d1809fa57aa07e2e05683
DaeYoung0726 <pdy000726@naver.com>
2025-02-28 17:07:04 +0900

    refact: dto명 변경으로 인한 반환형 수정

--- src/main/java/project/yourNews/domains/notification/service/NotificationService.java
+++ src/main/java/project/yourNews/domains/notification/service/NotificationService.java
@@ -3,7 +3,7 @@ package project.yourNews.domains.notification.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import project.yourNews.common.utils.redis.RedisUtil;
-import project.yourNews.domains.notification.dto.NewsNotificationDto;
+import project.yourNews.domains.notification.dto.NewsListDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,18 +20,18 @@ public class NotificationService {
     public void saveNewsInfo(String newsName, String postTitle, String postURL) {
 
         String key = NEWS_INFO_KEY_PREFIX + newsName;
-        NewsNotificationDto newsNotificationDto = new NewsNotificationDto(postTitle, postURL);
+        NewsListDto newsListDto = new NewsListDto(postTitle, postURL);
 
-        redisUtil.setList(key, newsNotificationDto);
+        redisUtil.setList(key, newsListDto);
     }
 
     /* 소식 종류 별 수신 정보 불러오기 */
-    public List<NewsNotificationDto> getAllNewsInfo(String newsName) {
+    public List<NewsListDto> getAllNewsInfo(String newsName) {
 
         String key = NEWS_INFO_KEY_PREFIX + newsName;
 
-        List<NewsNotificationDto> newsInfoList = redisUtil.getList(key).stream()
-                .map(obj -> (NewsNotificationDto) obj)
+        List<NewsListDto> newsInfoList = redisUtil.getList(key).stream()
+                .map(obj -> (NewsListDto) obj)
                 .collect(Collectors.toList());
 
         redisUtil.del(key);
